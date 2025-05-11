5b536028bed497291116ef8df9b3d3f553994b70
tranchauu <ITDSIU24011@student.hcmiu.edu.vn>
2025-05-11 11:41:24 +0700

    Add files via upload

---
+++ src/Plant.java
@@ -0,0 +1,13 @@
+import java.awt.*;
+
+public class Plant {
+    protected int x, y;
+
+    public Plant(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void draw(Graphics g, Component observer) {
+    }
+}
