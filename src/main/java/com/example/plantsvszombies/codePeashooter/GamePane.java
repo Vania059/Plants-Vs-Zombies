bc7cc984f37a53c3039047de870e510d43089879
Vania059 <hongyu05926@gmail.com>
2025-05-20 15:56:50 +0700

    add main class

---
+++ src/main/java/com/example/plantsvszombies/codePeashooter/GamePane.java
@@ -0,0 +1,15 @@
+package com.example.plantsvszombies.codePeashooter;
+
+import javafx.scene.layout.Pane;
+
+public class GamePane extends Pane {
+    private final Peashooter peashooter;
+
+    public GamePane() {
+        peashooter = new Peashooter();
+        getChildren().add(peashooter.getImageView());
+
+        peashooter.getImageView().setTranslateX(100);
+        peashooter.getImageView().setTranslateY(150);
+    }
+}
